4487042b4d11484c6ea8caff278b6adde3beb7ad
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-05-04 16:17:40 +0300

    Creating a form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+      const [name,setName] = useState('');
+      const [age,setAge] = useState(0);
+      const [height,setHeight] = useState(0);
+      const [superPower,setSuperPower] = useState('');
+
+
   return (
     <div className="App">
-      
+      <h1 style={{textAlign: 'center'}}>Build A Hero </h1>
+      <label>Name: </label>
+      <input type="text" style={{margin: 5}} 
+      onChange={(event) => 
+      {
+        setName(event.target.value)
+        }}
+        /> 
+      <label>Age: </label>
+      <input type="number" style={{margin: 5}}
+      onChange={(event) => 
+      {
+        setAge(event.target.value)
+        }}
+        /> 
+      <label>Height: </label>
+      <input type="number"  style={{margin: 5}}
+         onChange={(event) => 
+      {
+        setHeight(event.target.value)
+        }}
+      />
+      <label>Superpower: </label>
+      <input type="text" style={{ margin: 5}}
+         onChange={(event) => 
+      {
+        setSuperPower(event.target.value)
+        }}
+      />
+      {name}
+      {age}
+      {height}
+      {superPower}
     </div>
   );
 }
