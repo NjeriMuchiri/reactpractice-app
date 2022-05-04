f78d4f83463bb66351c46e832c8d65fa71041b3e
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-05-04 18:41:31 +0300

    Creating the todolist functions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Hero from './Components/Hero';
 import TodoList from './Components/TodoList';
+import './App.css';
 
 function App() {
   return (
     <>
+    <div className='App'>
     <div>
       <Hero />
     </div>
    <div>
      <TodoList />
+   </div>
    </div>
     </>
   )
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -7,7 +7,13 @@ function TodoList() {
   return (
       <>
     <div className='todo_sect'>
-      <h1>Todo List</h1>
+      <h1 className='todo__header'>Todo List</h1>
+      <div className='todo__task'>
+          <input type="text" placeholder='Task...'/>
+          <button className='todo__button'>Add Task</button>
+      </div>
+      <hr />
+
     </div>
       </>
   )
