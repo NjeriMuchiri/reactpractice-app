f8e8b5804701052583fa10a23ef85376e527bb9e
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-05-05 12:50:52 +0300

    Final commit on my todolist app

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -5,13 +5,25 @@ function TodoList() {
    const[todoList,setTodoList] = useState([]);
    const [currentTask, setCurrentTask] = useState('');
 
-   const inputTask = useRef(null)
+   const inputTask = useRef(null);
 
    const addTask = () =>{
-    setTodoList([...todoList, currentTask])
+    setTodoList([...todoList, {task: currentTask, completed:false}])
     inputTask.current.value = "  ";
     setCurrentTask('')
-   }
+   };
+
+     const clearTask = (taskToClear) => {
+       setTodoList(todoList.filter((task) => {
+         return task.task !== taskToClear;
+       }));
+     };
+
+      const doneTask = (taskDone) => {
+       setTodoList(todoList.map((task) => {
+         return task.task === taskDone ? {task: taskDone, completed:true} : {task: task.task,completed: task.completed ? true : false};
+       }));
+     };
 
   return (
       <>
@@ -20,6 +32,8 @@ function TodoList() {
       <div className='todo__task'>
           <input 
           ref = {inputTask}type="text" placeholder='Task...'
+          onKeyDown={(event) => { if (event.keyCode === 13 ) {addTask()}
+        }}
           onChange={(event) =>{
               setCurrentTask(event.target.value)
           }}
@@ -28,10 +42,22 @@ function TodoList() {
           onClick = {addTask} className='todo__button'>Add Task</button>
       </div>
       <ul className='todolist__list'>
-        {todoList.map((val, index) =>{
-          return <li key = {index}>
-            {val}
+        {todoList.map((val, key) =>{
+          return (
+          <div id="our_task">
+          <li key = {key}>
+            {val.task}
           </li>
+          <button
+          onClick={() => doneTask(val.task)}
+          className='tododone_button'>Done</button>
+          <button className='toclear_button'
+          onClick={() => clearTask(val.task)}
+          >Clear</button>
+          {val.completed ? 
+          (<h1 className='donetask'>Task is done</h1> ): (<h1 className='donetask'>Task is not done</h1>)}
+          </div>
+          )
         })}
       </ul>
     </div>
