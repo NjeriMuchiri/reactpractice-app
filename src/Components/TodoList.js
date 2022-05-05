b8e1bf859177a28c4fed27de990dd324546b09da
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-05-05 09:42:13 +0300

    Creating the todolst functions

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './TodoList.css';
 
 function TodoList() {
+   const[todoList,setTodoList] = useState([]);
+   const [currentTask, setCurrentTask] = useState('');
 
+
+   const addTask = () =>{
+    setTodoList([...todoList, currentTask])
+   }
 
   return (
       <>
     <div className='todo_sect'>
       <h1 className='todo__header'>Todo List</h1>
       <div className='todo__task'>
-          <input type="text" placeholder='Task...'/>
-          <button className='todo__button'>Add Task</button>
+          <input type="text" placeholder='Task...'
+          onChange={(event) =>{
+              setCurrentTask(event.target.value)
+          }}
+          />
+          <button 
+          onClick = {addTask} className='todo__button'>Add Task</button>
       </div>
       <hr />
-
     </div>
       </>
   )
